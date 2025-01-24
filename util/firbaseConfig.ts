30239dfeb2a89617d838f8f15b013d7cf3c30b85
nitishsaikumark <nitishsaikumarkadiyam@gmail.com>
2025-01-24 11:14:32 -0700

    Source Code

---
+++ util/firbaseConfig.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAH1yJAHnTlUNNeQjHZrm5wEVXcmCVMUhY',
+  authDomain: 'instagram-950c5.firebaseapp.com',
+  projectId: 'instagram-950c5',
+  storageBucket: 'instagram-950c5.firebasestorage.app',
+  messagingSenderId: '1003595070037',
+  appId: '1:1003595070037:web:7bdf7723bd8678b588b2dc',
+  measurementId: 'G-8FLGBE40QE'
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
